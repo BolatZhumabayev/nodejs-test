b23f4339231c96deadfd09397b93cc9a05e293e0
Bulat Zhumabayev <bolat.zh@gmail.com>
2019-12-04 22:56:48 +0600

    added phaser script

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require("express");
 
 var app = express();
-
+const port = 880;
 app.use(express.static('public'));
 //make way for some custom css, js and images
-app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/js', express.static(__dirname + '/public/js'));
-app.use('/images', express.static(__dirname + '/public/images'));
+//app.use('/css', express.static(__dirname + '/public/css'));
+//app.use('/js', express.static(__dirname + '/public/js'));
+//app.use('/img', express.static(__dirname + '/public/img'));
 
 var server = app.listen(8081, function(){
     var port = server.address().port;
+	console.log("1");
     console.log("Server started at http://localhost:%s", port);
 });
